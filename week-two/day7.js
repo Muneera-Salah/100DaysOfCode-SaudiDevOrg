89c0febb3f5f91337acb6529aa8b6e8de3d93212
Muneera-Salah <muneera.salah@gmail.com>
2019-08-27 00:25:33 +0300

    day 7

---
+++ week-two/day7.js
@@ -0,0 +1,1 @@
+var exampleForReservedWords = "break, delete, function, return, typeof, case, do, if, switch, var, catch, else, in, this, void, continue, false, instanceof, throw, while, debugger, finally, new, true, with, default, for, null, try , class, const, enum, export, extends, import, super .. etc" ;
