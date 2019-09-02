8dc9802fb5ce0c1a9b8015929035af4a45ab44ed
Muneera-Salah <muneera.salah@gmail.com>
2019-09-02 08:57:00 +0300

    day14

---
+++ week-three/day14.js
@@ -0,0 +1,9 @@
+var a = 0;
+if (a == 5)
+    document.write(a);
+else (a !== 5)
+    document.write(a);
+
+var age, voteable;
+voteable = (age < 18) ? "Too young" : "Old enough";
+document.write(voteable);
