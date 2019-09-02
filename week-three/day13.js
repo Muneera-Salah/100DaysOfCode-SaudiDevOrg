93c6a85842e6e33777fbfe2949f1b3c625fbf5b5
Muneera-Salah <muneera.salah@gmail.com>
2019-09-02 08:53:03 +0300

    day13

---
+++ week-three/day13.js
@@ -0,0 +1,7 @@
+var x = "muneera";
+document.write(x.length);
+var s = 'muneera salah';
+document.write(s.charAt(0));
+document.write(s.charAt(s.length-1));
+document.write(s.substring(1,4));
+document.write(s.slice(1,4));
